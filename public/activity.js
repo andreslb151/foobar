a5e3af6a504090eddcc6a099461fb95f060addb6
Andres Linares <alinares@devosinc.com>
2021-12-20 17:49:12 -0500

    mod5

--- public/activity.js
+++ public/activity.js
@@ -1,10 +1,10 @@
 var connection = new Postmonger.Session();
+const tagin = new Tagin(document.querySelector('#txtCampos'), { });
 
 // Startup Sequence
 connection.trigger('ready');
 
 connection.on('initActivity', function( data ) {
-  const tagin = new Tagin(document.querySelector('#txtCampos'), { });
   document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2 );
 });
 
