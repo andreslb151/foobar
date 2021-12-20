4b3b73d186182dc340a235b12c219283dbc49d5c
Andres Linares <alinares@devosinc.com>
2021-12-20 17:20:22 -0500

    mod2

--- public/activity.js
+++ public/activity.js
@@ -1,20 +1,14 @@
 var connection = new Postmonger.Session();
-var cfg = "";
 
 // Startup Sequence
 connection.trigger('ready');
 
 connection.on('initActivity', function( data ) {
-  // document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2 );
-  cfg = JSON.stringify( data, null, 2 );
-  alert(cfg);
+  document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2 );
 });
 
 // Save Sequence
 connection.on('clickedNext', function() {
-  // var configuration = JSON.parse( document.getElementById( 'configuration' ).value );
+  var configuration = JSON.parse( document.getElementById( 'configuration' ).value );
   
-  
-  cfg["metaData"]["isConfigured"] = true;
-  connection.trigger( 'updateActivity', cfg );
 });
