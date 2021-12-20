3ee1c1edf83003de164292352e5c777d700f16ff
Andres Linares <alinares@devosinc.com>
2021-12-20 16:48:10 -0500

    Creando nueva interfaz

--- public/activity.js
+++ public/activity.js
@@ -4,6 +4,7 @@ var connection = new Postmonger.Session();
 connection.trigger('ready');
 
 connection.on('initActivity', function( data ) {
+  alert(data);
   document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2 );
 });
 
